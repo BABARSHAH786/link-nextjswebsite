dfb175c267f5b57fa603da571af56d0ba5f89612
Syed Babar Ali <159919873+BABARSHAH786@users.noreply.github.com>
2024-10-23 19:37:16 +0500

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,8 +3,7 @@ export default function NavBar(){
   return (
     
     <div className="flex items-center justify-center text-3xl mt-5">
-       throw new Error('Prouct page not found')
-     
+
       </div>
    
   )
